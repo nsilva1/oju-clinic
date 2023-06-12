a83db9098d15138edb21aa20543a69d924bd2eb9
benjaminihentuge <osondubi@byui.edu>
2023-06-12 12:17:20 +0100

    latest

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import oju_clinics from '../../assets/oju_clinic.png';
+import glasses from '../../assets/Glasses (1).jpg';
+import eye_test from '../../assets/eye_test.jpg';
+
+
 const NavBar = () => {
   return(
         <div>
@@ -21,7 +25,7 @@ const NavBar = () => {
           <li>  
               <a>
                 <div>
-                <footer className="footer p-10 bg-white  text-green-800 w-[80rem] h-[100%]">
+                <footer className="footer p-10 bg-white  text-green-800 w-[50rem] h-[100%]">
                   <div>
                     <strong className="footer-title">Ages and Gender</strong> 
                     <a className="link link-hover">Women</a>
@@ -40,23 +44,10 @@ const NavBar = () => {
                     <a className="link link-hover">The LUXe Collection</a>
 
                   </div> 
-                  <div>
-                    <span className="footer-title">Guides</span> 
-                    <a className="link link-hover">Find your glasses</a>
-                    <a className="link link-hover">Buyer's guide</a>
-                    <a className="link link-hover">Online ordering</a>
-                    <a className="link link-hover">Guide to lenses</a>
-                    <a className="link link-hover">lens extra and treatments</a>
-                    <a className="link link-hover">Do blue light glasses work?</a>
-                    <a className="link link-hover">Order replacement glasses</a>
-                    <a className="link link-hover">aaccessories</a>
-                    <a className="link link-hover">#Loveglasses blog</a>
-                    <a className="link link-hover">Do I Need Glasses</a>
-
-                  </div>
-                  <div>
-                  <img src={oju_clinics} alt="Company Logo" className="w-[500px] h-[500px] " />
-     
+                
+                  <div className="normal-case items-center menu ">
+                  <img src={glasses} alt="Company Logo"  className="w-[250px] h-[250px]  " />
+                  <a className="glassesDiv btn border-none normal-case h-[1px] w-[200px] text-[#b5f44a] bg-white hover:bg-white text-[20px]"><h2>View Spec</h2></a>
                   </div>
         
                   
@@ -71,47 +62,33 @@ const NavBar = () => {
         </ul>
       </div>
       <div className="dropdown dropdown-hover">
-      <label tabIndex={0} className=" m-1 text-[20px] hover:bg-white  text-black bg-white ">Glasses</label>
+      <label tabIndex={0} className=" m-1 text-[20px] hover:bg-white  text-black bg-white ">Eye Test</label>
       <ul tabIndex={0} className="dropdown-content menu p-0 text-green-800 bg-base-500">
       <li>  
               <a>
                 <div>
-                <footer className="footer p-10 bg-white  text-green-800 w-[80rem] h-[100%]">
+                <footer className="footer p-10 bg-white  text-green-800 w-[40rem] h-[100%]">
                   <div>
-                    <strong className="footer-title">Ages and Gender</strong> 
-                    <a className="link link-hover">Women</a>
-                    <a className="link link-hover">Men</a>
-                    <a className="link link-hover">Unisex</a>
-                    <a className="link link-hover">Kids</a>
-                    <a className="link link-hover">Harry Potter Collection</a>
+                    <strong className="footer-title">Services</strong> 
+                    <a className="link link-hover">Book an eye test</a>
+                    <a className="link link-hover">Eyecare at work</a>
+                    <a className="link link-hover">OCT Scan</a>
+        
 
                   </div> 
                   <div>
-                    <span className="footer-title">Type</span> 
+                    <span className="footer-title">Product type</span> 
                     <a className="link link-hover">Sunglasses</a>
-                    <a className="link link-hover">designer lasses</a>
-                    <a className="link link-hover">Designer brands</a>
-                    <a className="link link-hover">Our glasses collection</a>
-                    <a className="link link-hover">The LUXe Collection</a>
+                    <a className="link link-hover">Monthly</a>
+                    <a className="link link-hover">Multifocal</a>
+                    <a className="link link-hover">Toric</a>
+                   
 
                   </div> 
-                  <div>
-                    <span className="footer-title">Guides</span> 
-                    <a className="link link-hover">Find your glasses</a>
-                    <a className="link link-hover">Buyer's guide</a>
-                    <a className="link link-hover">Online ordering</a>
-                    <a className="link link-hover">Guide to lenses</a>
-                    <a className="link link-hover">lens extra and treatments</a>
-                    <a className="link link-hover">Do blue light glasses work?</a>
-                    <a className="link link-hover">Order replacement glasses</a>
-                    <a className="link link-hover">aaccessories</a>
-                    <a className="link link-hover">#Loveglasses blog</a>
-                    <a className="link link-hover">Do I Need Glasses</a>
-
-                  </div>
-                  <div>
-                  <img src={oju_clinics} alt="Company Logo" className="w-[500px] h-[500px] " />
-     
+          
+                  <div className="normal-case items-center menu ">
+                  <img src={eye_test} alt="Company Logo"  className="w-[250px] h-[250px]  " />
+                  <a className="glassesDiv btn border-none normal-case h-[1px] w-[200px] text-[#b5f44a] bg-white hover:bg-white text-[20px]"><h2>Book now</h2></a>
                   </div>
         
                   
@@ -128,48 +105,33 @@ const NavBar = () => {
 
     
       <div className="dropdown dropdown-hover">
-        <label tabIndex={0} className=" m-1 text-[20px] hover:bg-white  text-black bg-white ">Glasses</label>
+        <label tabIndex={0} className=" m-1 text-[20px] hover:bg-white  text-black bg-white ">Hearing</label>
         <ul tabIndex={0} className="dropdown-content menu p-0 text-green-800 bg-base-500">
         <li>  
               <a>
                 <div>
-                <footer className="footer p-10 bg-white  text-green-800 w-[80rem] h-[100%]">
+                <footer className="footer p-10 bg-white  text-green-800 w-[40rem] h-[100%]">
                   <div>
-                    <strong className="footer-title">Ages and Gender</strong> 
-                    <a className="link link-hover">Women</a>
-                    <a className="link link-hover">Men</a>
-                    <a className="link link-hover">Unisex</a>
-                    <a className="link link-hover">Kids</a>
-                    <a className="link link-hover">Harry Potter Collection</a>
+                    <strong className="footer-title">Services</strong> 
+                    <a className="link link-hover">Book a free hearing test</a>
+                    <a className="link link-hover">Hearing test</a>
+                    <a className="link link-hover">Earwax removal</a>
+                    <a className="link link-hover">Hearing aid aftercare</a>
+                    <a className="link link-hover">Free online hearing test</a>
+
 
                   </div> 
                   <div>
-                    <span className="footer-title">Type</span> 
-                    <a className="link link-hover">Sunglasses</a>
-                    <a className="link link-hover">designer lasses</a>
-                    <a className="link link-hover">Designer brands</a>
-                    <a className="link link-hover">Our glasses collection</a>
-                    <a className="link link-hover">The LUXe Collection</a>
+                    <span className="footer-title">What happens in a hearing test</span> 
+                    <a className="link link-hover">All about earwax</a>
+                    <a className="link link-hover">All about ear health</a>
+                    <a className="link link-hover">All about PRSI and Medical Cards</a>
+                    <a className="link link-hover">Understanding hearing loss</a>
+                    <a className="link link-hover">Download an information sheet</a>
 
                   </div> 
-                  <div>
-                    <span className="footer-title">Guides</span> 
-                    <a className="link link-hover">Find your glasses</a>
-                    <a className="link link-hover">Buyer's guide</a>
-                    <a className="link link-hover">Online ordering</a>
-                    <a className="link link-hover">Guide to lenses</a>
-                    <a className="link link-hover">lens extra and treatments</a>
-                    <a className="link link-hover">Do blue light glasses work?</a>
-                    <a className="link link-hover">Order replacement glasses</a>
-                    <a className="link link-hover">aaccessories</a>
-                    <a className="link link-hover">#Loveglasses blog</a>
-                    <a className="link link-hover">Do I Need Glasses</a>
-
-                  </div>
-                  <div>
-                  <img src={oju_clinics} alt="Company Logo" className="w-[500px] h-[500px] " />
-     
-                  </div>
+                
+                  
         
                   
                 
@@ -189,7 +151,7 @@ const NavBar = () => {
         <li>  
               <a>
                 <div>
-                <footer className="footer p-10 bg-white  text-green-800 w-[80rem] h-[100%]">
+                <footer className="footer p-10 bg-white  text-green-800 w-[30rem] h-[100%]">
                   <div>
                     <strong className="footer-title">Ages and Gender</strong> 
                     <a className="link link-hover">Women</a>
@@ -208,24 +170,8 @@ const NavBar = () => {
                     <a className="link link-hover">The LUXe Collection</a>
 
                   </div> 
-                  <div>
-                    <span className="footer-title">Guides</span> 
-                    <a className="link link-hover">Find your glasses</a>
-                    <a className="link link-hover">Buyer's guide</a>
-                    <a className="link link-hover">Online ordering</a>
-                    <a className="link link-hover">Guide to lenses</a>
-                    <a className="link link-hover">lens extra and treatments</a>
-                    <a className="link link-hover">Do blue light glasses work?</a>
-                    <a className="link link-hover">Order replacement glasses</a>
-                    <a className="link link-hover">aaccessories</a>
-                    <a className="link link-hover">#Loveglasses blog</a>
-                    <a className="link link-hover">Do I Need Glasses</a>
-
-                  </div>
-                  <div>
-                  <img src={oju_clinics} alt="Company Logo" className="w-[500px] h-[500px] " />
-     
-                  </div>
+                 
+                  
         
                   
                 
@@ -243,7 +189,7 @@ const NavBar = () => {
       
   </div>
   <div className="navbar-end">
-  <a className="appointmentDiv btn  normal-case text-white bg-green-900 hover:bg-green-600 text-[25px]"><h2>Book appointment</h2></a>
+  <a className="appointmentDiv btn  normal-case text-white bg-[#b5f44a] hover:bg-[#b5f44a] text-[25px]"><h2>Book appointment</h2></a>
   </div>
 </div>
 
