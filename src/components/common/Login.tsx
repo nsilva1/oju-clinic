c0eaa3079f633e31c0159c31f8e36b4795f3af68
benjaminihentuge <osondubi@byui.edu>
2023-06-25 21:51:58 +0100

    25-06-2023

--- src/components/common/Login.tsx
+++ src/components/common/Login.tsx
@@ -1,27 +1,41 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 export const Login = (props: { onFormSwitch: (arg0: string) => void; }) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
-    console.log(email);
-  }
+    setLoading(true);
+
+    try {
+
+      console.log('Logging in...');
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      console.log('Login successful!');
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div className='auth-form-container'>
-      <h2>Login</h2>
-       <form className='login-form' onSubmit={handleSubmit}>
-            <label htmlFor="email">Staff Id</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email'/>
-            <label htmlFor="password">Password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder='********' id='email' name='password'/>
-            <button className='formbtn'  type='submit'>Login</button>
-       </form>
-       <button className='formbtn' onClick={()=> props.onFormSwitch('register') }> Dont have an account? Register here</button>
+    <h2>Login</h2>
+    <form onSubmit={handleSubmit}>
+    <fieldset className='login-form' disabled={loading}>
+      <label htmlFor="email">Staff ID</label>
+      <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email' />
+      <label htmlFor="password">Password</label>
+      <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder='********' id='password' name='password' />
+      <button className='formbtn' type='submit'>Login</button>
+    </fieldset>
+    </form>
+    <button className='formbtn' onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here</button>
     </div>
- 
-  )
-}
+  );
+};
 
 export default Login;
--- src/components/common/Register.tsx
+++ src/components/common/Register.tsx
@@ -1,38 +1,46 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
 
 export const Register = (props: { onFormSwitch: (arg0: string) => void; }) => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [managerCode, setManagerCode] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [managerCode, setManagerCode] = useState('');
+  const [loading, setLoading] = useState(false);
 
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    setLoading(true);
 
-
-    const handleSubmit = (e: { preventDefault: () => void; }) => {
-        e.preventDefault();
-        console.log(email);
-
+    try {
+      console.log('Registering user...');
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      console.log('User registered successfully!');
+    } catch (error) {
+      console.error('Registration failed:', error);
     }
 
+    setLoading(false);
+  };
 
-    return (
-        <div className='auth-form-container'>
-        <h2>Register</h2>
-        <form className='register-form' onSubmit={handleSubmit}>
-             <label htmlFor="id">Full name</label>
-             <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder='Full name' id='name' name='name'/>
-             <label htmlFor="email">Email</label>
-             <input value={email} onChange={(e) => setEmail(e.target.value)}type="Email" placeholder='johndoe@gmail.com' id='' name='email'/>
-             <label htmlFor="email">phoneNumber</label>
-             <input value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}type="text" placeholder='(+234) 800000000' id='number' name='number'/>
-             <label htmlFor="email">Manager Code</label>
-             <input value={managerCode} onChange={(e) => setManagerCode(e.target.value)}type="password" placeholder='*******' id='managerCode' name='managerCode'/>
-             <button className='formbtn' type='submit'>Login</button>
-        </form>
-        <button className='formbtn' onClick={()=> props.onFormSwitch('login')}>Already have an account? Login here.</button>
-     </div>
-  )
-}
+  return (
+    <div className='auth-form-container'>
+      <h2>Register</h2>
+      <form  onSubmit={handleSubmit}>
+        <fieldset className='register-form' disabled={loading}>
+          <label htmlFor="id">Full name</label>
+          <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder='Full name' id='name' name='name' />
+          <label htmlFor="email">Email</label>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} type="Email" placeholder='johndoe@gmail.com' id='email' name='email' />
+          <label htmlFor="email">Phone number</label>
+          <input value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} type="text" placeholder='(+234) 800000000' id='number' name='number' />
+          <label htmlFor="email">Manager Code</label>
+          <input value={managerCode} onChange={(e) => setManagerCode(e.target.value)} type="password" placeholder='*******' id='managerCode' name='managerCode' />
+          <button className='formbtn' type='submit'>Register</button>
+        </fieldset>
+      </form>
+      <button className='formbtn' onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+    </div>
+  );
+};
 
 export default Register;
