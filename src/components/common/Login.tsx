9758d3527ce91d4103a830b6d255e73806504341
Neto <ukpongneto@wellahealth.com>
2023-06-27 14:08:42 +0100

    format authentication files

--- src/components/common/Login.tsx
+++ src/components/common/Login.tsx
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 
-export const Login = (props: { onFormSwitch: (arg0: string) => void; }) => {
+export const Login = (props: { onFormSwitch: (arg0: string) => void }) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     setLoading(true);
 
     try {
-
       console.log('Logging in...');
       await new Promise((resolve) => setTimeout(resolve, 2000));
       console.log('Login successful!');
@@ -23,17 +22,38 @@ export const Login = (props: { onFormSwitch: (arg0: string) => void; }) => {
 
   return (
     <div className='auth-form-container'>
-    <h2>Login</h2>
-    <form onSubmit={handleSubmit}>
-    <fieldset className='login-form' disabled={loading}>
-      <label htmlFor="email">Staff ID</label>
-      <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email' />
-      <label htmlFor="password">Password</label>
-      <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder='********' id='password' name='password' />
-      <button className='formbtn' type='submit'>Login</button>
-    </fieldset>
-    </form>
-    <button className='formbtn' onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here</button>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <fieldset className='login-form' disabled={loading}>
+          <label htmlFor='email'>Staff ID</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type='email'
+            placeholder='youremail@gmail.com'
+            id='email'
+            name='email'
+          />
+          <label htmlFor='password'>Password</label>
+          <input
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            type='password'
+            placeholder='********'
+            id='password'
+            name='password'
+          />
+          <button className='formbtn' type='submit'>
+            Login
+          </button>
+        </fieldset>
+      </form>
+      <button
+        className='formbtn'
+        onClick={() => props.onFormSwitch('register')}
+      >
+        Don't have an account? Register here
+      </button>
     </div>
   );
 };
--- src/components/common/Register.tsx
+++ src/components/common/Register.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
-export const Register = (props: { onFormSwitch: (arg0: string) => void; }) => {
+export const Register = (props: { onFormSwitch: (arg0: string) => void }) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [managerCode, setManagerCode] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     setLoading(true);
 
@@ -25,20 +25,52 @@ export const Register = (props: { onFormSwitch: (arg0: string) => void; }) => {
   return (
     <div className='auth-form-container'>
       <h2>Register</h2>
-      <form  onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <fieldset className='register-form' disabled={loading}>
-          <label htmlFor="id">Full name</label>
-          <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder='Full name' id='name' name='name' />
-          <label htmlFor="email">Email</label>
-          <input value={email} onChange={(e) => setEmail(e.target.value)} type="Email" placeholder='johndoe@gmail.com' id='email' name='email' />
-          <label htmlFor="email">Phone number</label>
-          <input value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} type="text" placeholder='(+234) 800000000' id='number' name='number' />
-          <label htmlFor="email">Manager Code</label>
-          <input value={managerCode} onChange={(e) => setManagerCode(e.target.value)} type="password" placeholder='*******' id='managerCode' name='managerCode' />
-          <button className='formbtn' type='submit'>Register</button>
+          <label htmlFor='id'>Full name</label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type='text'
+            placeholder='Full name'
+            id='name'
+            name='name'
+          />
+          <label htmlFor='email'>Email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type='Email'
+            placeholder='johndoe@gmail.com'
+            id='email'
+            name='email'
+          />
+          <label htmlFor='email'>Phone number</label>
+          <input
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            type='text'
+            placeholder='(+234) 800000000'
+            id='number'
+            name='number'
+          />
+          <label htmlFor='email'>Manager Code</label>
+          <input
+            value={managerCode}
+            onChange={(e) => setManagerCode(e.target.value)}
+            type='password'
+            placeholder='*******'
+            id='managerCode'
+            name='managerCode'
+          />
+          <button className='formbtn' type='submit'>
+            Register
+          </button>
         </fieldset>
       </form>
-      <button className='formbtn' onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+      <button className='formbtn' onClick={() => props.onFormSwitch('login')}>
+        Already have an account? Login here.
+      </button>
     </div>
   );
 };
